1a39644d10d833be41a3f101673d1270a9059a21
henrymanyonyi <henrymanyonyi4@gmail.com>
2026-01-18 04:13:18 +0300

    django urls and views

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -46,7 +46,8 @@ print("Library Retrieved:", buruburu_library.name)
 print("Books in", library_name + ":", list(buruburu_library.books.all()))
 
 # 3. Retrieve the librarian for a library.
-buruburu_librarian = library1.librarian
-library_name = "Buruburu Library"
-librarian_name = Librarian.objects.get(Library = library_name)
+# buruburu_librarian = library1.librarian
+# Retrieve the librarian for a library by querying
+buruburu_librarian = Librarian.objects.get(library=library1)
 print("Librarian of Buruburu Library:", buruburu_librarian.name)
+print("Librarian Retrieved:", buruburu_librarian.name)
