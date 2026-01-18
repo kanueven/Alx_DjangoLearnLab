8fbef5f9fbeef1ca6224dcd2f1b624b9fcf33d28
henrymanyonyi <henrymanyonyi4@gmail.com>
2026-01-18 05:25:11 +0300

    authentification in django practice

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, login_view, logout_view, register_view
+from .views import list_books, LibraryDetailView, LoginView,LogoutView
+from . import views
 
 urlpatterns = [
     path('', list_books, name='home'),  # home page shows all books
     path('books/', list_books, name='book-list'),  # function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),  # class-based view
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('register/', register_view, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'), 
 ]
