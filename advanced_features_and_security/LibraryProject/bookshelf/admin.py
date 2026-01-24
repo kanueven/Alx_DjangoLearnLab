a630f42bf6f6f7a083988dde6e06e5d8aed33f1e
henrymanyonyi <henrymanyonyi4@gmail.com>
2026-01-25 01:17:03 +0300

    run migrations

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
@@ -10,3 +12,9 @@ class BookAdmin(admin.ModelAdmin):
     
 admin.site.register(Book, BookAdmin)
 
+#register custom user model
+@admin.register(CustomUser)
+class ModelAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Information', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
