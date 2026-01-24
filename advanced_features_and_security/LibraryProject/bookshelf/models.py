1a278cf436fe9ba29e1ac335d90ed8c6a2e767a1
henrymanyonyi <henrymanyonyi4@gmail.com>
2026-01-25 01:20:14 +0300

    run migrations

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -9,19 +9,6 @@ class Book(models.Model):
     publication_year = models.IntegerField()
     def __str__(self):
         return self.title
-    
-#custmize User Profile
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(
-        upload_to='profile_photos/',
-        null=True,
-        blank=True
-    )
-    
-    def __str__(self):
-        return self.username
-    
 
 class UserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
@@ -45,3 +32,18 @@ class UserManager(BaseUserManager):
             raise ValueError("Superuser must have is_superuser=True.")
 
         return self.create_user(username, email, password, **extra_fields)
+
+#custmize User Profile
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(
+        upload_to='profile_photos/',
+        null=True,
+        blank=True
+    )
+    objects = UserManager()
+    
+    def __str__(self):
+        return self.username
+    
+
