e15cba192fc1a16eb7f5b67189b30af29fc35801
henrymanyonyi <henrymanyonyi4@gmail.com>
2026-02-01 02:00:11 +0300

    Added API endpoint using django Rest framework

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -5,3 +5,6 @@ class Book(models.Model):
     title = models.CharField(max_length = 200)
     author = models.CharField(max_length = 100)
     published_date = models.DateField()
+    
+    def __str__(self):
+        return self.title
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.generics import  ListCreateAPIView
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
+class BookList(ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
