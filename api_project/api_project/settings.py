a1467be6274d1d5014a47fb60d5db0c597d102a8
henrymanyonyi <henrymanyonyi4@gmail.com>
2026-02-01 02:58:59 +0300

    Secure BookViewSet with token auth and permissions

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -32,6 +32,10 @@ RESTFRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    
 }
 
 
