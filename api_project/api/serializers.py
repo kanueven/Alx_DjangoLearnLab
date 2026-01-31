6c8def6cdd9501686b4852c053b56127eda8c551
henrymanyonyi <henrymanyonyi4@gmail.com>
2026-02-01 02:03:32 +0300

    Added API endpoint using django Rest framework

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -3,5 +3,6 @@ from .models import Book
 
 
 class BookSerializer(serializers.ModelSerializer):
-    model = Book
-    fields = ['id', 'title', 'author', 'published_date']
+    class Meta:
+            model = Book
+            fields = ['id', 'title', 'author', 'published_date']
