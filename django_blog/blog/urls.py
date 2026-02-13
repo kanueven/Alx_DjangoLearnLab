d5b639cb3081805e53b0ed4c50c3b20af94f56a5
Kanueven <raelrakeli@gmail.com>
2026-02-13 03:57:44 +0300

    feat(search and tagg): add post tagging via django-taggit and keyword search with Q objects

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     
      # Comment URLs
-    path('post/<int:pk>/comment/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
     
