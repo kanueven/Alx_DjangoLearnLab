71b8a9de7ce6424c3fdfaa9c62b18efa3844009b
Kanueven <raelrakeli@gmail.com>
2026-02-14 13:32:22 +0300

    Ensure static files (CSS, JS) are properly linked and served

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'blog'/'staticfiles'
+STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR/'blog'/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
