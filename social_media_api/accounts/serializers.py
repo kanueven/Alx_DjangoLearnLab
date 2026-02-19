b59537f9e335dac8c8ace6633dfde5ef67e590ea
Kanueven <raelrakeli@gmail.com>
2026-02-19 14:15:28 +0300

    custom user model, and token authentification, and a followers system

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', '')
+        )
+        Token.objects.create(user=user)
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        from django.contrib.auth import authenticate
+
+        user = authenticate(
+            username=data['username'],
+            password=data['password']
+        )
+
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+
+        data['user'] = user
+        return data
+
+
+class UserSerializer(serializers.ModelSerializer):
+    followers_count = serializers.SerializerMethodField()
+    following_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'email',
+            'bio',
+            'profile_picture',
+            'followers_count',
+            'following_count'
+        ]
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)d7nzlyyu(h!w2)nb=wq8ijoz_8kan*$a-3u5@1bi!4*k^cn9q'
+SECRET_KEY = 'django-insecure-p+!@*tqc2n@&8sr^ed@9w@tki_4=wysg$v6$5jy^)$=#m--jd9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,22 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'rest_framework.authtoken',
     'rest_framework',
     'accounts',
-    
+    'rest_framework.authtoken',
 ]
-
-AUTH_USER_MODEL = 'accounts.User'
-
-RESTFRAMEWORK = {
-    'DEFAULT_AUTHENTIFICATION_CLASSES' :[
-        'rest_framework.authenitification.TokenAuthentification',],
-    
-    'DEFAULT_PERMISSION_CLASSES':[
-        'rest_framework.permissions.isAuthenticated',
-    ]
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
